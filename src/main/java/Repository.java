7864bab65db3e93b73893192e946ffbd3ab31929
emmikoivisto <emetsi@gmail.com>
2020-11-20 15:38:31 +0000

    repository class and test created

--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Repository {
+
+    private String repositoryName;
+    private String description;
+    private ArrayList<Commit> commits;
+    private RepositoryType repository;
+
+    public Repository(String repositoryName, String description, RepositoryType repository){
+        this.repositoryName = repositoryName;
+        this.description = description;
+        this.repository = repository;
+    }
 }
--- src/test/java/RepositoryTest.java
+++ src/test/java/RepositoryTest.java
@@ -1,2 +1,11 @@
+import org.junit.Before;
+
 public class RepositoryTest {
+
+    Repository newRepository;
+
+    @Before
+    public void before(){
+        newRepository = new Repository("Githublab", "Java practice lab", RepositoryType.PRIVATE);
+    }
 }
